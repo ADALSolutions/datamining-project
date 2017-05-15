8270cbf8e31858106bdf48fbe5c915637764ab49
Pezze18 <dallepezzedavide@gmail.com>
2017-05-15 18:58:08 +0200

    SecondoRemoteCommit

--- runner_ProjectMain.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./gradlew compileJava
-export CP=$(./gradlew showDepsClasspath | grep jar)
-java -Dspark.master=local -cp $CP:build/classes/main it.unipd.dei.dm1617.ProjectMain small-sample.dat.bz2
-
